55feb38035ee9aa79160e144884c79a81b5ab201
unknown <ali.hamdani.developer@gmail.com>
2024-12-14 18:23:24 -0800

    getting somehwere

--- init.el
+++ init.el
@@ -13,6 +13,17 @@
 (setq visible-bell t)
 
 (load-theme 'wombat)
+;;Functions 
+;easy init reload
+(defun reload-init-file ()
+  "Reload the Emacs init file."
+  (interactive)
+  (load-file user-init-file))
+;easy init open
+(defun open-init-file ()
+  "Open the Emacs init file in a new buffer."
+  (interactive)
+  (find-file user-init-file))
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -38,25 +49,43 @@
 ;; Download keychord
 (unless (package-installed-p 'key-chord)
   (package-install 'key-chord))
+;; project files
+(require 'project)
+(defun my-project-current ()
+  "Show the current project root directory in the minibuffer."
+  (interactive)
+  (let ((project (project-current)))
+    (if project
+        (message "Current project root: %s" (project-root project))
+      (message "No project found."))))
+
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
+;; define evil keys, since i want both hands to access the key. i will set it to ctrl
 ;;key chord will make creating custom mappings a lot easier
 (require 'key-chord)
 (key-chord-mode 1)
-(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
-(key-chord-define evil-replace-state-map "jj" 'evil-normal-state)
-;startup page
-(setq initial-buffer-choice "/Users/alihamdani/Google Drive/My Drive/shurukaro/shurukaro/PROJECT_RIB_TASKS.md")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(grip-mode markdown-mode evil-collection)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(evil-define-key 'normal 'global (kbd "C-f") 'find-file)
+(evil-define-key 'normal 'global (kbd "C-r") 'reload-init-file)
+(evil-define-key 'normal 'global (kbd "C-w") 'save-buffer)
+(evil-define-key 'normal 'global (kbd "C-b") 'previous-buffer)
+(evil-define-key 'normal 'global (kbd "C-n") 'next-buffer)
+;;(key-chord-define evil-replace-state-map "jj" 'evil-normal-state)
+;; org shit
+(setq org-directory "G:\\My Drive\\magistrate\\")  
+(setq org-default-notes-file (expand-file-name "tasks.org" org-directory))
+(setq org-agenda-files (list (expand-file-name "tasks.org" org-directory)))
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+(setq org-capture-templates
+      '(("t" "Todo" entry
+         (file+headline org-default-notes-file "Tasks")
+         "* TODO %?\n  %u\n  %a")
+        ("n" "Note" entry
+         (file+headline org-default-notes-file "Notes")
+         "* %?\n  %u\n  %a")))
+;; TODO change this keybinding to make it consistent with everything else
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+;; startup page make it shurukaro
